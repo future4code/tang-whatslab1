d54c8532655a5d2ed6972945308ea33c86094b8f
Paula Soares <sou@paulasoares.me>
2020-10-22 16:40:18 -0300

    Tentando função Enviar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React from 'react'
+import Mensagem from './components/Mensagem'
 
 export default class App extends React.Component {
   state = {
@@ -11,27 +11,34 @@ export default class App extends React.Component {
   digitaNome = (event) => {
     this.setState({
       nome:event.target.value
-
     })
   }
 
   digitaMensagem = (event) => {
     this.setState({
       mensagem:event.target.value
-
-    })
-  
+    })  
   }
-
+  
   Enviar = () => {
-    mensagemEnviada = <Mensagem/>
-
+    this.setState({
+      enviado : true
+    })
   }
 
   render () {
+    
+  let mensagemEnviada
+
+  if (this.state.enviado) { 
+    mensagemEnviada = <Mensagem/>
+  }
+  
   return (
-    <div className="App">
-    <Mensagem/>
+    <div className="App">    
+    {mensagemEnviada}
+    <p>{this.state.nome}</p>
+    <p>{this.state.mensagem}</p>
     <div className="Entrada">
     <input className="nome" value= {this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
     <input className="mensagem" value={this.state.mensagem} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
