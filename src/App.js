7215550c8b1572c2af452803569b33f9c0be03ca
Bruna Pacheco <brunappacheco@hotmail.com>
2020-10-22 15:11:51 -0400

    Criando mensagem

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React from 'react';
 
 
 export default class App extends React.Component {
+  state = {
+    nome: "",
+    mensagem: "",
+    enviado: false,
+  }
+
   digitaNome = (event) => {
+    this.setState({
+      nome:event.target.value
+
+    })
+  }
+
+  digitaMensagem = (event) => {
+    this.setState({
+      mensagem:event.target.value
+
+    })
+  
+  }
+
+  Enviar = () => {
+    mensagemEnviada = <Mensagem/>
 
   }
+
   render () {
   return (
     <div className="App">
-    <p>oi</p>
+    <Mensagem/>
     <div className="Entrada">
-    <input className="nome" value="" onChange={digitaNome}></input>
-    <input></input>
-     <button>Enviar</button>
+    <input className="nome" value= {this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
+    <input className="mensagem" value={this.state.mensagem} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
+     <button onClick={this.Enviar}>Enviar</button>
      </div>
     </div>
   );
