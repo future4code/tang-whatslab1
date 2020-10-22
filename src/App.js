c479c072eaf3f8df5b010827784d191458c380f7
Bruna Pacheco <brunappacheco@hotmail.com>
2020-10-22 14:25:00 -0400

    criação inputs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+export default class App extends React.Component {
+  digitaNome = (event) => {
+
+  }
+  render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>oi</p>
+    <div className="Entrada">
+    <input className="nome" value="" onChange={digitaNome}></input>
+    <input></input>
+     <button>Enviar</button>
+     </div>
     </div>
   );
 }
+}
 
-export default App;
+
