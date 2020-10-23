38c5986a93b1b9a1a22af44c5a6cdb602ce33969
Bruna Pacheco <brunappacheco@hotmail.com>
2020-10-23 16:51:53 -0400

    tentei fazer o botão enviar funcionar e fiz um pouco de estilizeção no css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import React from 'react';
-
+import React from "react";
 
 export default class App extends React.Component {
   state = {
     nome: "",
-    mensagem: "",
-    enviado: false,
-  }
+    digiteMensagem: ""
+  };
 
   digitaNome = (event) => {
     this.setState({
-      nome:event.target.value
+      nome: event.target.value
+    });
+  };
 
-    })
+  onChangeInputMensagem = (event) => {
+    this.setState({ digiteMensagem: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Entrada">
+          <p>{this.state.digiteMensagem}</p>
+          <input
+            className="nome"
+            value={this.state.nome}
+            onChange={this.digitaNome}
+            placeholder={"Nome"}
+          ></input>
+
+          <input
+            className="mensagem"
+            value={this.state.digiteMensagem}
+            onChange={this.onChangeInputMensagem}
+            placeholder={"Digite a mensagem"}
+          ></input>
+
+          <button onClick={this.digiteMensagem}>Enviar</button>
+        </div>
+      </div>
+    );
   }
-
-  digitaMensagem = (event) => {
-    this.setState({
-      mensagem:event.target.value
-
-    })
-  
-  }
-
-  Enviar = () => {
-    mensagemEnviada = <Mensagem/>
-
-  }
-
-  render () {
-  return (
-    <div className="App">
-    <Mensagem/>
-    <div className="Entrada">
-    <input className="nome" value= {this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
-    <input className="mensagem" value={this.state.mensagem} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
-     <button onClick={this.Enviar}>Enviar</button>
-     </div>
-    </div>
-  );
 }
-}
-
-
