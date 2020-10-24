a550745ec0cf31f88ab52a3a9d721eda28f0d02f
Paula Soares <sou@paulasoares.me>
2020-10-24 11:24:27 -0300

    O enviar tá funcionando \o/ + css básico do histórico de mensagem

--- src/App.js
+++ src/App.js
@@ -4,49 +4,54 @@ import Mensagem from './components/Mensagem'
 export default class App extends React.Component {
   state = {
     nome: "",
-    mensagem: "",
-    enviado: false,
+    msg: "",
+    historico: [],
   }
 
   digitaNome = (event) => {
     this.setState({
-      nome:event.target.value
+      nome: event.target.value
     })
   }
 
   digitaMensagem = (event) => {
     this.setState({
-      mensagem:event.target.value
-    })  
-  }
-  
-  Enviar = () => {
-    this.setState({
-      enviado : true
+      msg: event.target.value
     })
   }
 
-  render () {
-    
-  let mensagemEnviada
+  Enviar = () => {
+    const novaMsg = {
+      nome: this.state.nome,
+      mensagem: this.state.msg
+    }
+    this.setState({
+      historico: [...this.state.historico, novaMsg]
+    })
 
-  if (this.state.enviado) { 
-    mensagemEnviada = <Mensagem/>
+    this.state.msg = ""
   }
-  
-  return (
-    <div className="App">    
-    {mensagemEnviada}
-    <p>{this.state.nome}</p>
-    <p>{this.state.mensagem}</p>
-    <div className="Entrada">
-    <input className="nome" value= {this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
-    <input className="mensagem" value={this.state.mensagem} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
-     <button onClick={this.Enviar}>Enviar</button>
-     </div>
-    </div>
-  );
-}
+
+  render() {
+    const historico = this.state.historico.map(
+      (msg) => {
+        return (
+          <Mensagem msg={msg} />
+        )
+      }
+    )
+
+    return (
+      <div className="App">
+        {historico}
+        <div className="Entrada">
+          <input className="nome" value={this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
+          <input className="mensagem" value={this.state.msg} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
+          <button onClick={this.Enviar}>Enviar</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 
