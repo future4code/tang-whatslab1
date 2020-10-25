6a50d5dc816181b4bf1668ac11e9e155d8416063
Bruna Pacheco <brunappacheco@hotmail.com>
2020-10-25 16:23:03 -0400

    passei para o styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
 import React from 'react'
 import Mensagem from './components/Mensagem'
+import styled from "styled-components";
+
+const InputNome = styled.input`
+  width: 15%;
+  font-weight: bold;
+  border: 2px solid rosybrown;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rosybrown;
+  padding: 5px;
+`;
+const InputMensagem = styled.input`
+  width: 70%;
+  font-weight: bold;
+  border: 2px solid rosybrown;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rosybrown;
+  padding: 5px;
+`;
+const BotaoEnviar = styled.button`
+  border: 2px solid rosybrown;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rosybrown;
+  padding: 5px;
+`;
+const CaixaTexto = styled.div`
+  border: 2px solid rosybrown;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rosybrown;
+  margin: auto;
+  padding: 5px;
+  width: 80vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+const CaixaEntrada = styled.div`
+  display: flex;
+  height: 6vh;
+  padding: 5px;
+  justify-content: space-between;
+`;
 
 export default class App extends React.Component {
   state = {
@@ -42,15 +84,24 @@ export default class App extends React.Component {
     )
 
     return (
-      <div className="App">
-
+      <CaixaTexto className="App">
         {historico}
-        <div className="Entrada">
-          <input className="nome" value={this.state.nome} onChange={this.digitaNome} placeholder={"Nome"}></input>
-          <input className="msg" value={this.state.msg} onChange={this.digitaMensagem} placeholder={"Digite a mensagem"}></input>
-          <button onClick={this.Enviar}>Enviar</button>
-        </div>
-      </div>
+        <CaixaEntrada>
+          <InputNome 
+            value={this.state.nome}
+            onChange={this.digitaNome}
+            placeholder={"Nome"}
+          ></InputNome>
+
+          <InputMensagem
+            value={this.state.msg}
+            onChange={this.digitaMensagem}
+            placeholder={"Digite a mensagem"}
+          ></InputMensagem>
+
+          <BotaoEnviar onClick={this.Enviar}>Enviar</BotaoEnviar>
+        </CaixaEntrada>
+      </CaixaTexto>
     );
   }
 }
